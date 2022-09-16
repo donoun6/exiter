8b928bb1f98df3a3ed66228fd5d20c118bb918c2
donoun6 <donoun6@naver.com>
2022-09-16 22:26:31 +0900

    table controller

---
+++ exiter/src/main/resources/TableController.sql
@@ -0,0 +1,15 @@
+DROP TABLE User;
+
+DROP TABLE Company;
+
+DROP TABLE Theme;
+
+DROP TABLE Reservation;
+
+DROP TABLE Board;
+
+DROP TABLE BoardComment;
+
+DROP TABLE Question;
+
+DROP TABLE Answer;
