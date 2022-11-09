ea773ba43d05dd5043172b18ba67adbf3b536c97
KIM NAHYE <goodhik8@daum.net>
2022-11-09 19:57:29 +0900

    reservation data update

--- exiter/src/main/resources/reservationData.sql
+++ exiter/src/main/resources/reservationData.sql
@@ -1,16 +1,16 @@
 -- 예약 임시데이터
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 1, 1, 125000, '2022-11-02', '11:55 AM', 5);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 1, 2, 42000, '2022-11-03', '01:40 PM', 2);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 2, 4, 80000, '2022-11-05', '10:40 AM', 4);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 3, 7, 105000, '2022-11-05', '03:55 PM', 6);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 2, 5, 52000, '2022-11-07', '11:55 AM', 2);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 3, 6, 72000, '2022-11-09', '06:25 PM', 4);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 1, 11, 60000, '2022-11-10', '03:05 PM', 3);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 1, 1, 125000, '2022-11-09', '11:55 AM', 5);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 2, 2, 42000, '2022-11-13', '01:40 PM', 2);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 4, 4, 80000, '2022-11-15', '10:40 AM', 4);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 7, 7, 105000, '2022-11-15', '03:55 PM', 6);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 5, 5, 52000, '2022-11-17', '11:55 AM', 2);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 6, 6, 72000, '2022-11-19', '06:25 PM', 4);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (1, 11, 11, 60000, '2022-11-20', '03:05 PM', 3);
 
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 3, 18, 90000, '2022-11-02', '03:05 PM', 5);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 2, 3, 46000, '2022-11-03', '01:00 PM', 2);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 3, 6, 72000, '2022-11-03', '07:40 PM', 4);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 1, 8, 60000, '2022-11-05', '06:15 PM', 3);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 2, 14, 60000, '2022-11-07', '01:30 PM', 3);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 1, 9, 105000, '2022-11-08', '07:50 PM', 6);
-INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 3, 22, 120000, '2022-11-09', '09:25 PM', 7);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 18, 18, 90000, '2022-11-09', '03:05 PM', 5);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 3, 3, 46000, '2022-11-13', '01:00 PM', 2);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 6, 6, 72000, '2022-11-13', '07:40 PM', 4);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 8, 8, 60000, '2022-11-15', '06:15 PM', 3);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 14, 14, 60000, '2022-11-17', '01:30 PM', 3);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 9, 9, 105000, '2022-11-18', '07:50 PM', 6);
+INSERT INTO Reservation(uid, cid, tid, rPrice, rDate, rTime, rNum) VALUES (2, 22, 22, 120000, '2022-11-19', '09:25 PM', 7);
